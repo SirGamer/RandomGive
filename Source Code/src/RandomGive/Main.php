97cfb820d13b48945d27a6bc322a43a3e6afe4c2
ZINGDING <tjgmltn07@naver.com>
2016-02-11 12:16:50 +0900

    DevelopMent Version Beta-0.1-Release

--- Source Code/src/RandomGive/Main.php
+++ Source Code/src/RandomGive/Main.php
@@ -10,6 +10,7 @@ use pocketmine\utils\TextFormat;
 
 
 class Main extends PluginBase implements Listener{
+	public $items, $config, $itemWithMeta;
 	public function onLoad() {
 		$this->getLogger()->info("§aThis plugin is RandomGive plugin.");
 		$this->getLogger()->info("§cMade By ZINGDING.");
@@ -37,7 +38,12 @@ class Main extends PluginBase implements Listener{
 		
 		$id = $player->getLevel ()->getBlockIdAt ( $x, $y, $z );
 		$data = $player->getLevel ()->getBlockDataAt ( $x, $y, $z );
-
+		//#30 ~ #37까지의 라인은 hm님의 Trampoline에서 복붙해옴. 제가 직접 코드 짜는 것 보단 잘 짜신 흠님 것을 님이 보고 공부하시는 것이
+		//도움이 될 듯 하여 올림. 절대 물외한인이 귀찮아서가 아님.
+		//참고로 위 소스 제대로 동작할지는 미지수.
+		//ㄹㅇ 노답 ㅂㄷㅂㄷ;;
+		
+		
 		switch($id) {
 			case 35:
 				if($data == 1) {
@@ -50,6 +56,27 @@ class Main extends PluginBase implements Listener{
 				break;
 			default:
 				break;
+		}
+		foreach($this->itemWithMeta as $key => $meta) {
+			switch($id) {
+				case $key:
+					if($meta == $data) {
+						$this->getInventory()->addItem(array_rand($this->items));
+					}
+			}
+		}
+	}
+	public function $loadConfig(){
+		$this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML, ["Enable" => true]))->getAll();
+		$blocks = (new Config($this->getDataFolder()."blocks.yml", Config::YAML, [0] = 35:1))->getALL();
+		$items = (new Config($this->getDataFolder()."items.yml", Config::YAML, [0] = ))->getALL();
+		foreach($items as $item) {
+			$itemArr = explode(":", $item);
+			$this->items = new Item(eval($itemArr[0]), $itemArr[1]);
+		}
+		foreach($blocks as $blocks) {
+			$blockArr = explode(":", $block)
+			$this->block[$blockArr[0]] = new Item(eval($blockArr[0]), $blockArr[1]);
 		}
 	}
 }
